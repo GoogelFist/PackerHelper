a6e3a929473a89f6b7bb786597b9579123012cb0
Nikita Gugel <googelsecond@gmail.com>
2022-12-15 14:09:49 +0500

    initial commit

---
+++ app/src/main/java/com/github/googelfist/packerhelper/MainApplication.kt
@@ -0,0 +1,10 @@
+package com.github.googelfist.packerhelper
+
+import android.app.Application
+
+class MainApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
---
+++ app/src/main/java/com/github/googelfist/packerhelper/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.github.googelfist.packerhelper.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.github.googelfist.packerhelper.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
