5b1d847829eb8c3fdbee34dcba879774349f28dc
Nikita Gugel <googelsecond@gmail.com>
2022-12-15 15:12:09 +0500

    add bottom tab fragment, add navigation

--- app/src/main/java/com/github/googelfist/packerhelper/presentation/MainActivity.kt
+++ app/src/main/java/com/github/googelfist/packerhelper/presentation/MainActivity.kt
@@ -2,11 +2,20 @@ package com.github.googelfist.packerhelper.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
 import com.github.googelfist.packerhelper.R
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                add<TabsFragment>(R.id.fragment_container)
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/github/googelfist/packerhelper/presentation/PalletFragment.kt
@@ -0,0 +1,7 @@
+package com.github.googelfist.packerhelper.presentation
+
+import androidx.fragment.app.Fragment
+import com.github.googelfist.packerhelper.R
+
+class PalletFragment : Fragment(R.layout.pallet_fragment) {
+}
