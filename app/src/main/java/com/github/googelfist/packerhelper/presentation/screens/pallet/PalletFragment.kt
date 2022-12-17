4cf8990a212e168a55247e2883aea5f77bba668e
Nikita Gugel <googelsecond@gmail.com>
2022-12-17 17:35:50 +0500

    add clear button

--- app/src/main/java/com/github/googelfist/packerhelper/presentation/screens/pallet/PalletFragment.kt
+++ app/src/main/java/com/github/googelfist/packerhelper/presentation/screens/pallet/PalletFragment.kt
@@ -53,79 +53,17 @@ class PalletFragment : Fragment(R.layout.pallet_fragment) {
     private fun observeViewModel() {
         viewModel.result.observe(viewLifecycleOwner) { state ->
             when (state) {
+                is PalletState.InitState -> setInitState(state)
                 is PalletState.Less -> setLessState(state)
                 is PalletState.Correct -> setCorrectState(state)
                 is PalletState.More -> setMoreState(state)
-                is PalletState.InitState -> {
-                    binding.textInputEditTextBoxCount.setText(state.boxCount.toString())
-                    binding.textInputEditTextPackageWeight.setText(state.packageWeight.toString())
-                }
             }
         }
     }
 
-    private fun configEditText() {
-
-        binding.textInputEditTextBoxWeight.doOnTextChanged { text, start, before, count ->
-            val boxWeight = if (text.toString().isEmpty() || !text.toString().first().isDigit()) {
-                DEFAULT_VALUE
-            } else {
-                text.toString()
-            }
-            viewModel.obtainEvent(PalletEvent.LoadPallet(boxWeight.toFloat()))
-        }
-
-        binding.textInputEditTextPackageWeight.setOnEditorActionListener { v, actionId, event ->
-            if (actionId == EditorInfo.IME_ACTION_DONE) {
-                binding.textInputEditTextPackageWeight.clearFocus()
-                v.hideKeyboard()
-            }
-            false
-        }
-    }
-
-    private fun setupButtons() {
-
-        binding.root.setOnClickListener {
-            it.hideKeyboard()
-            requireActivity().currentFocus?.clearFocus()
-        }
-
-        binding.buttonCalculate.setOnClickListener {
-
-            with(binding) {
-                validateEditText(textInputEditTextBoxWeight)
-                validateEditText(textInputEditTextTrayWeight)
-                validateEditText(textInputEditTextGrossWeight)
-                validateEditText(textInputEditTextBoxCount)
-                validateEditText(textInputEditTextPackageWeight)
-
-                val boxWeight = textInputEditTextBoxWeight.text.toString()
-                val trayWeight = textInputEditTextTrayWeight.text.toString()
-                val grossWeight = textInputEditTextGrossWeight.text.toString()
-                val boxCount = textInputEditTextBoxCount.text.toString()
-                val packageWeight = textInputEditTextPackageWeight.text.toString()
-
-                viewModel.obtainEvent(
-                    PalletEvent.CalculateRealGross(
-                        boxWeight = boxWeight.toFloat(),
-                        trayWeight = trayWeight.toFloat(),
-                        grossWeight = grossWeight.toFloat(),
-                        boxCount = boxCount.toInt(),
-                        packageWeight = packageWeight.toFloat()
-                    )
-                )
-
-                root.hideKeyboard()
-                requireActivity().currentFocus?.clearFocus()
-            }
-        }
-    }
-
-    private fun validateEditText(editText: TextInputEditText) {
-        if (editText.text.toString().isEmpty() || !editText.text.toString().first().isDigit()) {
-            editText.setText("0")
-        }
+    private fun setInitState(state: PalletState.InitState) {
+        binding.textInputEditTextBoxCount.setText(state.boxCount.toString())
+        binding.textInputEditTextPackageWeight.setText(state.packageWeight.toString())
     }
 
     private fun setLessState(state: PalletState.Less) {
@@ -164,6 +102,94 @@ class PalletFragment : Fragment(R.layout.pallet_fragment) {
                 getString(R.string.more_real_gross_message, state.realGross, state.diff)
 
             tvRealGross.setTextColor(Color.RED)
+        }
+    }
+
+    private fun configEditText() {
+
+        binding.textInputEditTextBoxWeight.doOnTextChanged { text, start, before, count ->
+            val boxWeight = if (text.toString().isEmpty() || !text.toString().first().isDigit()) {
+                DEFAULT_VALUE
+            } else {
+                text.toString()
+            }
+            viewModel.obtainEvent(PalletEvent.LoadPallet(boxWeight.toFloat()))
+        }
+
+        binding.textInputEditTextPackageWeight.setOnEditorActionListener { v, actionId, event ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                binding.textInputEditTextPackageWeight.clearFocus()
+                v.hideKeyboard()
+            }
+            false
+        }
+    }
+
+    private fun setupButtons() {
+
+        binding.root.setOnClickListener {
+            it.hideKeyboard()
+            requireActivity().currentFocus?.clearFocus()
+        }
+
+        setupCalculateButton()
+
+        setupClearButton()
+    }
+
+    private fun setupClearButton() {
+        with(binding) {
+
+            buttonClear.setOnClickListener {
+                textInputEditTextBoxWeight.setText("")
+                textInputEditTextTrayWeight.setText("")
+                textInputEditTextGrossWeight.setText("")
+                textInputEditTextBoxCount.setText("")
+                textInputEditTextPackageWeight.setText("")
+
+                tvTheoreticalGross.text = ""
+                tvClearWeight.text = ""
+                tvAllow.text = ""
+                tvRealGross.text = ""
+            }
+        }
+    }
+
+    private fun setupCalculateButton() {
+        binding.buttonCalculate.setOnClickListener {
+
+            with(binding) {
+                validateEditText(textInputEditTextBoxWeight)
+                validateEditText(textInputEditTextTrayWeight)
+                validateEditText(textInputEditTextGrossWeight)
+                validateEditText(textInputEditTextBoxCount)
+                validateEditText(textInputEditTextPackageWeight)
+
+                val boxWeight = textInputEditTextBoxWeight.text.toString()
+                val trayWeight = textInputEditTextTrayWeight.text.toString()
+                val grossWeight = textInputEditTextGrossWeight.text.toString()
+                val boxCount = textInputEditTextBoxCount.text.toString()
+                val packageWeight = textInputEditTextPackageWeight.text.toString()
+
+                viewModel.obtainEvent(
+                    PalletEvent.CalculateRealGross(
+                        boxWeight = boxWeight.toFloat(),
+                        trayWeight = trayWeight.toFloat(),
+                        grossWeight = grossWeight.toFloat(),
+                        boxCount = boxCount.toInt(),
+                        packageWeight = packageWeight.toFloat()
+                    )
+                )
+
+                root.hideKeyboard()
+                requireActivity().currentFocus?.clearFocus()
+            }
+        }
+    }
+
+    private fun validateEditText(editText: TextInputEditText) {
+        if (editText.text.toString().isEmpty() || !editText.text.toString().first().isDigit()) {
+            editText.setText("0")
         }
     }
 
